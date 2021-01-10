0fe46557c2a4f898dd3716d7d544e9da6cf40619
thomaslapadat <thomaslapadat@gmail.com>
2021-01-10 12:45:27 -0800

    Finished Group API endpoints

--- backend/controllers/group.js
+++ backend/controllers/group.js
@@ -1,8 +1,96 @@
-// const Group = require('../models/group');
+const Group = require('../models/group');
+const User = require('../models/user');
 
 module.exports = {
-    getGroups: (req, res) => {
-        console.log("Here is a group placeholder!");
+    getGroup: (req, res) => {
+        try {
+            const myGroup = Group.findById(req.params.id).catch(() => null)
+
+            if (myGroup) {
+                res.status(200).send(myGroup);
+            }
+            else {
+                res.status(404).send({"message": "No groups exist with that id"});
+            }
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send();
+        }
         res.status(200).send({"message": "Good"});
+    },
+    createGroup: async (req, res) => {
+        const name = req.body.name;
+        const creator = req.headers.email;
+        const participants = [];
+        const lists = [];
+        const transactions = [];
+
+        const newGroup = {
+            name,
+            creator,
+            participants,
+            lists,
+            transactions
+        }
+
+        try {
+            await Group.create(newGroup, (err, result) => {
+              res.status(201).send(result);  
+            })
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send();
+        }
+    },
+    addUserToGroup: async (req, res) => {
+        const myUser = User.findById(req.body.id).catch(() => null);
+
+        if (myUser) {
+            const myGroup = Group.findById(req.params.id).catch(() => null);
+            if (myGroup) {
+                await Group.findByIdAndUpdate(req.params.id, {
+                    $push: { participants: req.body.id }
+                });
+                res.status(200).send();
+            }
+            else {
+                res.status(404).send({"message": "Group with that id does not exist"});
+            }
+        }
+        else {
+            res.status(404).send({"message": "User with that id does not exist"});
+        }
+
+    },
+    removeUserFromGroup: async (req, res) => {
+        const myUser = User.findById(req.body.id).catch(() => null);
+
+        if (myUser) {
+            const myGroup = Group.findById(req.params.id).catch(() => null);
+            if (myGroup) {
+                await Group.findByIdAndUpdate(req.params.id, {
+                    $pull: { participants: req.body.id }
+                });
+                res.status(200).send();
+            }
+            else {
+                res.status(404).send({"message": "Group with that id does not exist"});
+            }
+        }
+        else {
+            res.status(404).send({"message": "User with that id does not exist"});
+        }
+    },
+    deleteGroup: async (req, res) => {
+        try {
+            await Group.findByIdAndDelete(req.params.id);
+            res.status(200).send();
+        }
+        catch (err) {
+            console.log(err);
+            res.status(500).send();
+        }
     }
 }
--- backend/routes/group.js
+++ backend/routes/group.js
@@ -3,6 +3,14 @@ const router = express.Router();
 
 const groupController = require('../controllers/group');
 
-router.get('/', groupController.getGroups);
+router.get('/:id', groupController.getGroup);
+
+router.post('/', groupController.createGroup);
+
+router.patch('/:id', groupController.addUserToGroup);
+
+router.patch('/:id', groupController.removeUserFromGroup);
+
+router.delete('/:id', groupController.deleteGroup);
 
 module.exports = router;
