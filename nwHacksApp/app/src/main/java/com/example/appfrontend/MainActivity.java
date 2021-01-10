9bb9a1bd6a2e228e3928bcc852dc20d72a754553
Jed <jed324@gmail.com>
2021-01-09 19:20:36 -0800

    listview stuff

--- nwHacksApp/app/src/main/java/com/example/appfrontend/MainActivity.java
+++ nwHacksApp/app/src/main/java/com/example/appfrontend/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.appfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("android");
+        arrayList.add("poo");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
     }
 }
