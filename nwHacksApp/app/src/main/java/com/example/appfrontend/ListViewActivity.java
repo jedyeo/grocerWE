e0d8d54ea80cc6789889e64dde4c11394b2b0992
Jed <jed324@gmail.com>
2021-01-09 19:59:32 -0800

    list view activity

---
+++ nwHacksApp/app/src/main/java/com/example/appfrontend/ListViewActivity.java
@@ -0,0 +1,30 @@
+package com.example.appfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ListViewActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        listView = findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("android");
+        arrayList.add("poo");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+    }
+}
