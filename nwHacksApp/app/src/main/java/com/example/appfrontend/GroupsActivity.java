be5117be90e6f6258ac2cdaa09d93d77d35a4dd1
calhep <callumahepworth@gmail.com>
2021-01-10 13:40:08 -0700

    Working groups activity.

--- nwHacksApp/app/src/main/java/com/example/appfrontend/GroupsActivity.java
+++ nwHacksApp/app/src/main/java/com/example/appfrontend/GroupsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,11 +20,38 @@ public class GroupsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_groups);
 
         ListView itemListView = findViewById(R.id.itemlistview);
+        Button addItemButton = findViewById(R.id.button6);
+        EditText textBox = findViewById(R.id.editTextTextPersonName2);
+
         ArrayList<String> itemList = new ArrayList<>();
         itemList.add("Wentworth Block");
         itemList.add("Household");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, itemList);
         itemListView.setAdapter(adapter);
+
+        // Listener to add items
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String item = textBox.getText().toString();
+
+                if (!item.equals("")) {
+                    itemList.add(textBox.getText().toString());
+                    textBox.setText("");
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        // Listener to remove items
+        itemListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                itemList.remove(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
     }
 }
