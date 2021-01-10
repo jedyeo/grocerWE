025cc171c19b61ba72f8ce3425a60b2a329f217e
calhep <callumahepworth@gmail.com>
2021-01-09 21:08:52 -0700

    Added groups activity, navigation in main activity.

--- nwHacksApp/app/src/main/java/com/example/appfrontend/MainActivity.java
+++ nwHacksApp/app/src/main/java/com/example/appfrontend/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 
@@ -12,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToGroups(View view) {
+        Intent intent = new Intent(this, GroupsActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToList(View view) {
+        Intent intent = new Intent(this, ListViewActivity.class);
+        startActivity(intent);
     }
 }
