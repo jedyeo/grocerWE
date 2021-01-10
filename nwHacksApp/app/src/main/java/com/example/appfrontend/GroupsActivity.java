e8b49a4c9c363a38a5eec57efa3ebc0edb075fd0
calhep <callumahepworth@gmail.com>
2021-01-10 13:30:15 -0700

    Added basic groups functionality.

--- nwHacksApp/app/src/main/java/com/example/appfrontend/GroupsActivity.java
+++ nwHacksApp/app/src/main/java/com/example/appfrontend/GroupsActivity.java
@@ -3,6 +3,12 @@ package com.example.appfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class GroupsActivity extends AppCompatActivity {
 
@@ -10,6 +16,13 @@ public class GroupsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
-    }
 
+        ListView itemListView = findViewById(R.id.itemlistview);
+        ArrayList<String> itemList = new ArrayList<>();
+        itemList.add("Wentworth Block");
+        itemList.add("Household");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, itemList);
+        itemListView.setAdapter(adapter);
+    }
 }
