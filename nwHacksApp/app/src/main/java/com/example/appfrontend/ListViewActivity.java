341c4c0dcb55f6864935524c95bec633be6d3d71
Jed <jed324@gmail.com>
2021-01-09 21:11:04 -0800

    scuffed list

--- nwHacksApp/app/src/main/java/com/example/appfrontend/ListViewActivity.java
+++ nwHacksApp/app/src/main/java/com/example/appfrontend/ListViewActivity.java
@@ -3,28 +3,42 @@ package com.example.appfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class ListViewActivity extends AppCompatActivity {
 
+    private ArrayList<String> itemList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
 
+        // Find view by ID
         ListView itemListView = findViewById(R.id.itemlistview);
         Button addItemButton = findViewById(R.id.additembutton);
+        EditText textBox = findViewById(R.id.inputtext);
 
-        ArrayList<String> itemList = new ArrayList<>();
+        // Instantiate new array list
+        itemList = new ArrayList<>();
+        
+        // Instantiate and create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, itemList);
+        itemListView.setAdapter(adapter);
 
-        itemList.add("android");
-        itemList.add("poo");
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, itemList);
-        itemListView.setAdapter(arrayAdapter);
+        // Listener to add items
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemList.add(textBox.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
